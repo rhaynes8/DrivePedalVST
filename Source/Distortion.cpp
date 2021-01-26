fbfeaa6ace06aa2c1b3f3c766d12d68760fc7fff
Richie Haynes <77641979+rhaynes8@users.noreply.github.com>
2021-01-26 20:57:32 +0000

    Oversampling added
    Oversampling added. Fixed 'Fuzz' Clipping. Added slight asymmetrical clipping in waveshaper

--- Source/Distortion.cpp
+++ Source/Distortion.cpp
@@ -14,11 +14,24 @@
 
 Distortion::Distortion()
 {
+    
     auto& waveshaper = processorChain.template get<waveshaperIndex>();
     waveshaper.functionToUse = [&, this] (float x)
 
     {
-        return (tanh(pow(10, this->getGain() / 20) * x))/tanh(pow(10, this->getGain() / 20));
+
+        if (x > 1.0f) {
+            return 1.0f;
+        } else if (x < -1.0f) {
+            return -1.0f;
+        } else if (x > 0.0f && x < 1.0f) {
+            return static_cast<float>((tanh(pow(10, this->getGain() / 20) * x))/tanh(pow(10, this->getGain() / 20)));
+            
+        } else if (x < 0.0f) {
+            return std::tanh(x);
+        } else {
+            return 0.0f;
+        }
  
         
     };
@@ -37,6 +50,8 @@ void Distortion::prepare (const juce::dsp::ProcessSpec& spec)
     filter.state = FilterCoefs::makeFirstOrderLowPass (spec.sampleRate, 3500.0f);
     filter.state = FilterCoefs::makeFirstOrderHighPass(spec.sampleRate, 50.0f);
     
+    oversampling.initProcessing(spec.maximumBlockSize);
+    
     processorChain.prepare (spec);
 }
 
@@ -44,13 +59,18 @@ void Distortion::prepare (const juce::dsp::ProcessSpec& spec)
 //template <typename ProcessContext>
 void Distortion::process (juce::dsp::ProcessContextReplacing<float> context) noexcept
 {
-    processorChain.process (context);
+    auto oversampledBlock = oversampling.processSamplesUp (context.getInputBlock());
+    
+    processorChain.process (juce::dsp::ProcessContextReplacing<float> (oversampledBlock));
+    
+    oversampling.processSamplesDown (context.getOutputBlock());
 }
 
 //==============================================================================
 void Distortion::reset() noexcept
 {
     processorChain.reset();
+    oversampling.reset();
 }
 
 void Distortion::setGain(float gainValue)
